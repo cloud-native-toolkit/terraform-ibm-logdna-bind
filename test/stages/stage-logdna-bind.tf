53d717b36eb4c6ef05bb74fe7b37693f1933647c
Sean Sundberg <seansund@us.ibm.com>
2021-04-03 08:26:15 -0500

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage-logdna-bind.tf
+++ test/stages/stage-logdna-bind.tf
@@ -3,6 +3,7 @@ module "logdna-bind" {
 
   resource_group_name      = var.resource_group_name
   region                   = var.region
+  ibmcloud_api_key         = var.ibmcloud_api_key
   cluster_id               = module.dev_cluster.id
   cluster_name             = module.dev_cluster.name
   cluster_config_file_path = module.dev_cluster.config_file_path
