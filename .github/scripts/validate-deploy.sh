d616eaa5c69482b607409a8984d71ff362a34356
Sean Sundberg <seansund@us.ibm.com>
2021-05-22 21:39:25 -0500

    Updates to fix cluster deployment (#10)
    - Adds tests to validate deployment of daemonset and consolelink
    - Updates metadata to synchronize execution between logdna and sysdig
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+KUBECONFIG=$(cat ./kubeconfig)
+
+echo "Checking for logdna-agent daemonset"
+if ! kubectl get daemonset logdna-agent -n ibm-observe; then
+  echo "logdna-agent daemonset not found"
+  exit 1
+fi
+
+echo "Checking logdna-agent pod status"
+if ! kubectl rollout status daemonset/logdna-agent -n ibm-observe; then
+  echo "daemonset/logdna-agent rollout status error"
+  exit 1
+fi
+
+echo "Checking for console link"
+if ! kubectl get consolelink toolkit-logdna; then
+  echo "consolelink not found"
+  kubectl get consolelink
+  exit 1
+fi
