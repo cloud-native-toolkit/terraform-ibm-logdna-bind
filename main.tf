7470b53d116e32cfb299ed694af4fc682e90ec0c
Sean Sundberg <seansund@us.ibm.com>
2022-05-11 23:25:43 -0500

    Updates handling of login
    closes #30
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -38,42 +38,39 @@ resource "ibm_resource_key" "logdna_instance_key" {
   }
 }
 
-resource null_resource ibmcloud_login {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/ibmcloud-login.sh ${var.region} ${var.resource_group_name}"
-
-    environment = {
-      BIN_DIR = module.setup_clis.bin_dir
-      APIKEY = var.ibmcloud_api_key
-    }
-  }
-}
-
 resource "null_resource" "logdna_bind" {
   count = local.bind ? 1 : 0
-  depends_on = [null_resource.ibmcloud_login]
 
   triggers = {
     bin_dir = module.setup_clis.bin_dir
     cluster_id  = var.cluster_id
     instance_id = var.logdna_id
+    region = var.region
+    resource_group = var.resource_group_name
+    ibmcloud_api_key = var.ibmcloud_api_key
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/bind-instance.sh ${self.triggers.cluster_id} ${self.triggers.instance_id} ${ibm_resource_key.logdna_instance_key[0].name} ${var.private_endpoint}"
+    command = "${path.module}/scripts/bind-instance.sh '${self.triggers.cluster_id}' '${self.triggers.instance_id}' '${ibm_resource_key.logdna_instance_key[0].name}' '${var.private_endpoint}'"
 
     environment = {
       BIN_DIR = self.triggers.bin_dir
       SYNC = var.sync
+      REGION = self.triggers.region
+      RESOURCE_GROUP = self.triggers.resource_group
+      IBMCLOUD_API_KEY = self.triggers.ibmcloud_api_key
     }
   }
 
   provisioner "local-exec" {
     when    = destroy
-    command = "${path.module}/scripts/unbind-instance.sh ${self.triggers.cluster_id} ${self.triggers.instance_id}"
+    command = "${path.module}/scripts/unbind-instance.sh '${self.triggers.cluster_id}' '${self.triggers.instance_id}'"
 
     environment = {
       BIN_DIR = self.triggers.bin_dir
+      REGION = self.triggers.region
+      RESOURCE_GROUP = self.triggers.resource_group
+      IBMCLOUD_API_KEY = self.triggers.ibmcloud_api_key
     }
   }
 }
