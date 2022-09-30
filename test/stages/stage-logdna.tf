4c308447b8c3358987612506894b8b2a5d41e953
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-30 10:00:11 -0500

    Move from CN org (#39)
    - Updates test setup to provision a new cluster each time
    
    Signed-off-by: Bala <@balasgit>
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage-logdna.tf
+++ test/stages/stage-logdna.tf
@@ -1,8 +1,8 @@
 module "logdna" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-log-analysis.git"
 
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group.name
   region                   = var.region
   provision                = true
-  name_prefix              = var.name_prefix
+  name_prefix              = local.name_prefix
 }
